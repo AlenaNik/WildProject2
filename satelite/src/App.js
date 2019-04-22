698e85895545464fa6707f37998aa327fb789002
AlenaNik <alena.nik.digital@gmail.com>
2019-04-22 21:19:31 +0200

    added more oprional styles to App js

--- satelite/src/App.js
+++ satelite/src/App.js
@@ -26,6 +26,9 @@ class App extends Component {
                 <TileLayer
                     attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     url="http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+                    // opcion en color url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                    // opcion negro url="http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+                    // opcion blanquita url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
                 />
 
             </Map>
