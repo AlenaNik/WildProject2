56d302a118976982d7a91df3ddc9e667f1a3b84b
AlenaNik <alena.nik.digital@gmail.com>
2019-05-06 23:39:28 +0200

    changed About us text to grey

--- satelite/src/Components/AboutUs.js
+++ satelite/src/Components/AboutUs.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
             <div className="tc">
-                    <h1 className="f3 mb2">International Space Station Current Location</h1>
+                    <h1 className="f3 mb2 black-70">International Space Station Current Location</h1>
                     <h2 className="f5 fw4 gray mt0">The International Space Station is moving at close to 28,000 km/h so its location changes really fast! Where is it right now?</h2>
             </div>
         </div>
